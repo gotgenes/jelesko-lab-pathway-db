6fb5667260121ba476aa3623b968fe2e23af092a
Chris Lasher <chris@shaun.local>
2009-03-18 13:05:01 -0400

    Starting script to parse FASTA to a flat file.

---
+++ fastatoflat/fastatoflat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+Converts an NCBI FASTA protein file to a flat file for database input.
+
+"""
+
+__author__ = 'Chris Lasher'
+__email__ = 'chris DOT lasher <AT> gmail DOT com'
+
