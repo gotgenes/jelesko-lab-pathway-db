3af3dd335f0be38f93669215413f138faf05d498
Chris Lasher <chris.lasher@gmail.com>
2009-04-19 12:56:51 -0400

    Starting command to allow creation of FASTA files from DB entries.

---
+++ Jelesko_Django/jelesko_web/blast_fasta/management/commands/dbtofasta.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+Dumps the proteins in the database to a FASTA formatted file.
+
+"""
+
+__author__ = 'Chris Lasher'
+__email__ = 'chris DOT lasher <AT> gmail DOT com'
+
