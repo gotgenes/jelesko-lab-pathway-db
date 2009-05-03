95465f7bc6aefb6d8e762328f714c9d13474cb23
Chris Lasher <chris.lasher@gmail.com>
2009-05-03 18:06:01 -0400

    Correcting the redirection.

--- Jelesko_Django/jelesko_web/blast_fasta/views.py
+++ Jelesko_Django/jelesko_web/blast_fasta/views.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
 from django import forms
@@ -446,7 +447,7 @@ def seqrequest(request):
 
     # redirect user to page containing links to these files
     return HttpResponseRedirect(
-        reverse('seqselection', args=[selection.id])
+        reverse(seqselection, args=[selection.id])
     )
 
 
